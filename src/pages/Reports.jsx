bd67fee57f65a1d11d2e4c80adb9af28a97e5282
eunjiro <marbillaeunjiro@gmail.com>
2025-08-06 01:47:52 +0800

    delete unecessary files

--- src/pages/Reports.jsx
+++
@@ -1,8 +0,0 @@
-export default function Reports() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4 dark:text-white">Reports</h1>
-      <p className="text-gray-600 dark:text-gray-300">View and generate reports</p>
-    </div>
-  )
-}
--- src/pages/Users.jsx
+++
@@ -1,8 +0,0 @@
-export default function Users() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4 dark:text-white">Users</h1>
-      <p className="text-gray-600 dark:text-gray-300">User management</p>
-    </div>
-  )
-}
